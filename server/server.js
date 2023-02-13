45ff0679cab8df32ab45c40a6f55ebcde3a46b97
Webie <90978643+vimal-oneway@users.noreply.github.com>
2023-02-13 15:26:29 +0530

    backend completed

--- server/server.js
+++ server/server.js
@@ -7,20 +7,3 @@ dotenv.config({path:path.join(__dirname,"/config/config.env")});
 const server = app.listen(process.env.PORT,()=>{
     console.log("server listening to the port ", process.env.PORT,"in ",process.env.NODE_ENV);
 });
-
-// process.on('unhandledRejection', (err)=>{
-//     console.log(`Error: ${err.message}`);
-//     console.log(`Shutting down the server due to unhanledRejection error`);
-//     server.close(()=>{
-//         process.exit(1);
-//     });
-// }) 
-
-// process.on('uncaughtException', (err)=>{
-//     console.log(`Error: ${err.message}`);
-//     console.log(`Shutting down the server due to uncaught error`);
-//     server.close(()=>{
-//         process.exit(1);
-//     });
-// })
-  
